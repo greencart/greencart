36cd6116347d231afc5927a3829b5a69416a0934
Sebastian Ionescu <sebastian.c.ionescu@gmail.com>
2011-05-09 23:18:47 +0300

    implemented a shortcut syntax for urls

--- greencart/Lib/View/Helper/AppHelper.php
+++ greencart/Lib/View/Helper/AppHelper.php
@@ -18,4 +18,53 @@ App::uses('Helper', 'View');
  */
 class AppHelper extends Helper
 {
+	/**
+	 * Finds URL for specified action.
+	 *
+	 * @param mixed $url A string or array-based URL.
+	 * @param bool $full If true, the full base URL will be prepended to the result.
+	 * @return string Full translated URL with base path.
+	 * @see Helper::url()
+	 */
+	public function url($url = null, $full = false)
+	{
+		if (is_string($url) && $url[0] === ':') {
+			$url = $this->__parseShortUrl($url);
+		} else if (isset($url[0][0]) && $url[0][0] === ':') {
+			$url = array_merge($url, $this->__parseShortUrl($url[0]));
+			unset($url[0]);
+		}
+
+		return parent::url($url, $full);
+	}
+
+	/**
+	 * Returns the current language.
+	 *
+	 * @return string
+	 */
+	public function lang()
+	{
+		return Configure::read('Config.language');
+	}
+
+	/**
+	 * Parses the short format notation for internal URLs.
+	 *
+	 * @param string $shortUrl
+	 * @return array
+	 */
+	private function __parseShortUrl($shortUrl)
+	{
+		$url      = array();
+		$params   = array('action', 'controller', 'plugin');
+		$shortUrl = array_reverse(explode('.', ltrim($shortUrl, ':')));
+
+		foreach ($shortUrl as $key => $value) {
+			$url[$params[$key]] = $value;
+		}
+
+		return $url;
+	}
+
 }
