3d6b46d030d70e46b0de1e5dc568a797494ba7d9
Sebastian Ionescu <sebastian.c.ionescu@gmail.com>
2011-05-10 13:40:41 +0300

    introduced GreenCart class

---
+++ greencart/Lib/GreenCart.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the GreenCart package.
+ *
+ * Copyright (c) 2011 Sebastian Ionescu
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * GreenCart Class
+ *
+ * @author Sebastian Ionescu <sebastian.c.ionescu@gmail.com>
+ */
+class GreenCart
+{
+	/**
+	 * Used to determine the current version of GreenCart.
+	 */
+	const VERSION = '1.0.0-DEV';
+}
