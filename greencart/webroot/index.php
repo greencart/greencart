b2198d64dad80a2617409b169140f6ed2b7a2fae
Sebastian Ionescu <sebastian.c.ionescu@gmail.com>
2011-07-23 16:49:12 +0300

    moved tmp directory outside of greencart

--- greencart/webroot/index.php
+++ greencart/webroot/index.php
@@ -34,14 +34,15 @@ define('APP_DIR', 'greencart');
 /**
  * The absolute path to the "cake" directory, WITHOUT a trailing DS.
  */
-define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendors' . DS . 'cakephp' . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH', ROOT.DS.'vendors'.DS.'cakephp'.DS.'lib');
 
 define('WEBROOT_DIR', 'webroot');
-define('WWW_ROOT',    dirname(__FILE__) . DS);
-define('APP_PATH',    ROOT . DS . APP_DIR . DS);
-define('CORE_PATH',   CAKE_CORE_INCLUDE_PATH . DS);
+define('WWW_ROOT',    dirname(__FILE__).DS);
+define('TMP',         ROOT.DS.'tmp'.DS);
+define('APP_PATH',    ROOT.DS.APP_DIR.DS);
+define('CORE_PATH',   CAKE_CORE_INCLUDE_PATH.DS);
 
-include(CORE_PATH . 'Cake' . DS . 'bootstrap.php');
+include(CORE_PATH.'Cake'.DS.'bootstrap.php');
 
 if (isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO'] == '/favicon.ico') {
 	return;
--- greencart/webroot/test.php
+++ greencart/webroot/test.php
@@ -34,20 +34,21 @@ define('APP_DIR', 'greencart');
 /**
  * The absolute path to the "cake" directory, WITHOUT a trailing DS.
  */
-define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendors' . DS . 'cakephp' . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH', ROOT.DS.'vendors'.DS.'cakephp'.DS.'lib');
 
 define('WEBROOT_DIR', 'webroot');
-define('WWW_ROOT',    dirname(__FILE__) . DS);
-define('APP_PATH',    ROOT . DS . APP_DIR . DS);
-define('CORE_PATH',   CAKE_CORE_INCLUDE_PATH . DS);
+define('WWW_ROOT',    dirname(__FILE__).DS);
+define('TMP',         ROOT.DS.'tmp'.DS);
+define('APP_PATH',    ROOT.DS.APP_DIR.DS);
+define('CORE_PATH',   CAKE_CORE_INCLUDE_PATH.DS);
 
-include(CORE_PATH . 'Cake' . DS . 'bootstrap.php');
+include(CORE_PATH.'Cake'.DS.'bootstrap.php');
 
 if (!Configure::read('debug')) {
 	header('HTTP/1.1 404 Not Found');
 	exit;
 }
 
-require_once CAKE . 'TestSuite' . DS . 'CakeTestSuiteDispatcher.php';
+require_once CAKE.'TestSuite'.DS.'CakeTestSuiteDispatcher.php';
 
 CakeTestSuiteDispatcher::run();
