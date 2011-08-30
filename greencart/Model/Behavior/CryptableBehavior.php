e5735c631140e4f57ee90ac1c690384ea0ffa6dc
Sebastian Ionescu <sebastian.c.ionescu@gmail.com>
2011-08-30 11:36:29 +0300

    updated docs in previous commit

--- greencart/Model/Behavior/CryptableBehavior.php
+++ greencart/Model/Behavior/CryptableBehavior.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the MobotixTools package.
+ * This file is part of the GreenCart package.
  *
- * Copyright (c) 2011 MobotixTools.com
+ * Copyright (c) 2011 Sebastian Ionescu
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
